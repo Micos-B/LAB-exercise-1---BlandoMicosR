76aba058a6876ed6facaf7429a2ab45718bace0e
Micos-B <micos8700@gmail.com>
2024-08-21 22:49:05 +0800

    LOOP

--- LAB EXCERCISE - BLANDO.cpp
+++ LAB EXCERCISE - BLANDO.cpp
@@ -5,11 +5,27 @@
 using namespace std;
 int main(){
 	
+	int UserInput;
+	
+	do{
+	
+	
+	
 	cout << "1. To Display List of Files " << endl;
 	cout << "2. To Create New Directory" << endl;
 	cout << "3. To Change the Working Directory" << endl;
 	cout << "4. Exit" << endl;
 	cout << "Enter the Number: ";
+	cin >> UserInput;
+	if(UserInput == 1){
+		
+	}
+	}while(UserInput != 4);
+	
+	
+	
+	
+	
 	
 	return 0;
 }
