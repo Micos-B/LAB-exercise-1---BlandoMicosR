2debec213e4fdb4526a31dfb3fa93654ea7e8abe
Micos-B <micos8700@gmail.com>
2024-08-21 16:30:02 +0800

    Command

--- LAB EXCERCISE - BLANDO.cpp
+++ LAB EXCERCISE - BLANDO.cpp
@@ -1,3 +1,15 @@
 #include <iostream>
-using namespace std;
 
+
+
+using namespace std;
+int main(){
+	
+	cout << "1. To Display List of Files " << endl;
+	cout << "2. To Create New Directory" << endl;
+	cout << "3. To Change the Working Directory" << endl;
+	cout << "4. Exit" << endl;
+	cout << "Enter the Number: ";
+	
+	return 0;
+}
