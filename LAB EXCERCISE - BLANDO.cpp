833d779722fe433899b94e2b6965741d634a9415
Micos-B <micos8700@gmail.com>
2024-08-22 08:29:00 +0800

    TRY2

--- LAB EXCERCISE - BLANDO.cpp
+++ LAB EXCERCISE - BLANDO.cpp
@@ -1,24 +1,25 @@
 #include <iostream>
 
-
-
 using namespace std;
+
+void Options();
+
+
+
+
+
 int main(){
 	
 	int UserInput;
 	
 	do{
-	
-	
-	
-	cout << "1. To Display List of Files " << endl;
-	cout << "2. To Create New Directory" << endl;
-	cout << "3. To Change the Working Directory" << endl;
-	cout << "4. Exit" << endl;
+	Options();
 	cout << "Enter the Number: ";
 	cin >> UserInput;
-	if(UserInput == 1){
-		
+	if(UserInput == 1){	
+	
+	
+	
 	}
 	}while(UserInput != 4);
 	
@@ -28,4 +29,12 @@ int main(){
 	
 	
 	return 0;
+}
+void Options(){
+	cout << "1. To Display List of Files " << endl;
+	cout << "2. To Create New Directory" << endl;
+	cout << "3. To Change the Working Directory" << endl;
+	cout << "4. Exit" << endl;
+	cout << "Enter the Number: ";
+	
 }
