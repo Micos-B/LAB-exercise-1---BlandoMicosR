f89d5fa2322245bf59d063f420a1be11af2c41bf
Micos-B <micos8700@gmail.com>
2024-08-25 12:11:09 +0800

    Making functions

--- LAB EXCERCISE - BLANDO.cpp
+++ LAB EXCERCISE - BLANDO.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
-
 using namespace std;
-
 void Options();
-
-
+void displayList();	
+void listAllfiles
 
 
 
@@ -13,14 +11,20 @@ int main(){
 	int UserInput;
 	
 	do{
+		
 	Options();
 	cout << "Enter the Number: ";
 	cin >> UserInput;
-	if(UserInput == 1){	
+	
+	if(UserInput == 1){
+	void displayList();
+		
 	
 	
 	
 	}
+	
+	
 	}while(UserInput != 4);
 	
 	
@@ -30,11 +34,19 @@ int main(){
 	
 	return 0;
 }
+
+
 void Options(){
 	cout << "1. To Display List of Files " << endl;
 	cout << "2. To Create New Directory" << endl;
 	cout << "3. To Change the Working Directory" << endl;
 	cout << "4. Exit" << endl;
 	cout << "Enter the Number: ";
+	
+}
+void displayList(){
+	cout << "1. List All Files " << endl;
+	cout << "2. List of Extension Files" << endl;
+	cout << "3. List if Name Wise" << endl;
 	
 }
